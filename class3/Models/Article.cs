4c64231563ef7f22bd91cc03112c69677fcd01ab
Alisha Pratik Patel <alishapatel79@gmail.com>
2020-01-29 08:43:06 -0500

    Added model

---
+++ class3/Models/Article.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace class3.Models
+{
+    public class Article
+    {
+        public string Author { get; set; }
+
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime Datecreated { get; set; }
+    }
+}
